b6eb68011142b98fee8a8ef4dad035631717791d
Ffoo66 <130569354+Ffoo66@users.noreply.github.com>
2024-05-19 16:23:24 +0200

    Update chat1.php

--- ProjetFlo/chat1.php
+++ ProjetFlo/chat1.php
@@ -7,26 +7,55 @@
 	</head>
 	<body>
 		<?php
-			function displaymessage($user){
-				$file = fopen('./chat.txt', 'r');
-				while (!feof($file)){
-					echo "<div id='ownmessage'>".fgets($file);
-					echo "<br></div>";
-				}
-				fclose($file);
-			}
-		
-			function sendmessage($user, $message){
-				if(isset($message)){
-					$file = fopen('./chat.txt', 'a');
-					fwrite($file, $user.";".trim($message, "\n\x0B \t\r"));
-					fclose($file);
+			function displaymessage($ownuser, $othuser){
+				if(file_exists('./chat'.$othuser.'.txt')){
+					$chat = fopen('./chat'.$othuser.'.txt', 'r');
+					while (!feof($chat)){
+						$tab = explode(";",fgets($chat));
+						if (feof($chat)){
+							break;
+						}
+						if ($tab[0] == $ownuser){
+							$tab2 = array_slice($tab, 1);
+							$message = implode($tab2);
+							echo "<div id='ownmessage'>".$message;
+							echo "<br></div>";
+						}
+						else if ($tab[0] == $othuser){
+							$tab2 = array_slice($tab, 1);
+							$message = implode($tab2);
+							echo "<div id='othmessage'>".$message;
+							echo "<br></div>";
+						}
+						else{
+							echo "Problem displaymessage";
+						}
+					}
+					fclose($chat);          
 				}
 			}
 			
-			sendmessage("1", $_POST["message"]);
+			function sendmessage($ownuser, $message, $othuser){
+				$owninfo = fopen('./1.txt', 'r');
+				$tab = explode(";", fgets($owninfo));
+				if(isset($message) && ($tab[3] == "subscribed" || $tab[3] == "admin")){
+					$trimmed = trim($message, "\n\x0B \t\r");
+					if (!empty($trimmed)){
+						$ownchat = fopen('./chat'.$othuser.'.txt', 'a');
+						fwrite($ownchat, $ownuser.";".$trimmed."\n");
+						fclose($ownchat);
+						$othchat = fopen('../'.$othuser.'/chat'.$ownuser.'.txt', 'a');
+						fwrite($othchat, $ownuser.";".$trimmed."\n");
+						fclose($othchat);
+					}
+				}
+			}
 			
-			displaymessage("1");
+			$othuser = '2';
+			
+			sendmessage("1", $_POST["message"], $othuser);
+			
+			displaymessage("1", $othuser);
 		?>
 		<form action="chat1.php" method="post" enctype="multipart/form-data" id="editprofile">
 			<textarea id=message name=message>
