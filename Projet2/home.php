8e4dded8e32c9a0e6366796ffff5da55a7058aae
chrmil <130459909+chrmil@users.noreply.github.com>
2024-05-24 14:46:38 +0200

    Update home.php

--- Projet2/home.php
+++ Projet2/home.php
@@ -13,14 +13,14 @@
     }   
     reset($users);
     $count=count($users);
-    $lastUsers=array(); //array of the last 5 new users 
+    $lastUsers=array(); //array of the last 10 new users 
     $i=0;
     
-    for($i=0; $i<5; $i++){ 
+    for($i=0; $i<10; $i++){ 
         $j=0;
         for($j=0;$j<$count-1;$j++){ //for each of the users in the list
             if(date_create_from_format("d/m/Y" , $users[$j][7])>date_create_from_format("d/m/Y" , $users[$j+1][7])){ //if the $j user's account creation date is more recent
-              foreach ($lastUsers as $user){ //for each of the last 5 new users
+              foreach ($lastUsers as $user){ //for each of the last 10 new users
                 if($user[0]!=$users[$j][0]){ //if the user isn't already in the array
                         $lastUsers[$i]=$users[$j]; //is added to the array
                     }
