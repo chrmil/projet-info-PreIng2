c737909f07ef090622e611760e899d9a4c618485
mareksedlacek1 <41283055+mareksedlacek1@users.noreply.github.com>
2024-05-25 21:06:24 +0200

    Update home.php
    age +sexe

--- Projet2/home.php
+++ Projet2/home.php
@@ -1,65 +1,54 @@
 <?php 
     session_start();
     include("users.php");   
-    $users=getUserlist(); //gets userlist, not global
+    $users = getUserlist(); // Obtient la liste des utilisateurs, pas global
     
     try {
         if(!isset($users) || empty($users)){
-                throw new Exception("Error: home.php : user list");
+            throw new Exception("Error: home.php : user list");
         }
         if(!isset($_SESSION["userID"]) || empty($_SESSION["userID"])){
             throw new Exception("Please log in to see the 10 last new trainers");
         }
-
-    
     }  
     catch(Exception $e){
-        echo $e->getMessage();
+        #echo $e->getMessage();
     }   
-    reset($users);
-    $lastUsers=array_fill(0, 10 , array_fill(0, 19, 0) ); //array of the last 10 new users 
-    $i=0;
-    $j=0;
-    $c=count($users);
-    for($i=0; $i<10 ; $i++ ){
-        for ($j=0; $j<$c ; $j++){
-            $t1=1;
-            for($k=0; $k<10 ; $k++ ){
-                $array=$users[$j];
-                if($array[0]==$lastUsers[$k][0]){//check $j isn't already in the array
-                    $t1=0;
+
+    $lastUsers = array_fill(0, 10, array_fill(0, 19, 0)); // Tableau des 10 derniers nouveaux utilisateurs 
+    $i = 0;
+    $j = 0;
+    $c = count($users);
+    for($i = 0; $i < 10; $i++) {
+        for ($j = 0; $j < $c; $j++) {
+            $t1 = 1;
+            for($k = 0; $k < 10; $k++) {
+                $array = $users[$j];
+                if($array[0] == $lastUsers[$k][0]) { // Vérifie que $j n'est pas déjà dans le tableau
+                    $t1 = 0;
                 }
             }
-            if($t1 && $lastUsers[$i][18]<$users[$j][18]){
-                $lastUsers[$i]=$users[$j];
+            if($t1 && $lastUsers[$i][18] < $users[$j][18]) {
+                $lastUsers[$i] = $users[$j];
             }
         }
     } 
-    $test=fopen("test1.txt", "w+");
-    foreach ($users as $user){
-        fputcsv($test, $user, ";");
-        fwrite($test, "\n");
-    }
-    fclose($test);  
-    $test=fopen("test2.txt", "w+");
-    foreach ($lastUsers as $user){
-        fputcsv($test, $user, ";");
-        fwrite($test, "\n");
-    }
-    fclose($test);       
-    $full=1; 
-    foreach ($lastUsers as $user){ //display the results
-        if($user[0]!=0){
-            echo "$user[1]\n"; //to modify in order to display the profiles more properly 
-        }
-        else{
-            $full=0;
+
+    // Tableau pour stocker les noms et les âges des utilisateurs
+    $userDetails = array();
+    foreach ($lastUsers as $user) {
+        if($user[0] != 0) {
+            $userDetails[] = array(
+                "nom" => $user[1], // Ajoute le nom de l'utilisateur
+                "age" => $user[2], // Ajoute l'âge de l'utilisateur
+                "sex" => $user[6]
+            );
         }
     }
-    if ($full==0){
-        echo "No other trainers have joined us yet.\n";
-    }
-    
-      
-   
+
+    // Convertit le tableau des noms et âges d'utilisateurs en JSON
+    $userDetailsJSON = json_encode($userDetails);
+
+    // Envoie le JSON à la page HTML
+    echo $userDetailsJSON;
 ?>
