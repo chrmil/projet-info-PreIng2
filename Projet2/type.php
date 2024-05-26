b968f1524af8a0a8dce68fb4393580f571eb356a
chrmil <130459909+chrmil@users.noreply.github.com>
2024-05-26 21:26:02 +0200

    Delete Projet2 directory

--- Projet2/type.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $type_color=array();
-    $type_color=array("empty" => "white","bug" => "green", "dark" => "black", "dragon" => "black", "electric" => "yellow", "fairy" =>"pink",	"fighting" => "pink",
-    	"fire" => "pink",	"flying"=>"blue",	"ghost"  => "black", "grass" => "green", "ground"=>"brown",
-		"ice" =>"white", "normal" =>"white", "poison" => "grey", "psychic" => "pink",	"rock" => "brown",	"steel"=>"grey",	"water" => "blue");
-
-?>
